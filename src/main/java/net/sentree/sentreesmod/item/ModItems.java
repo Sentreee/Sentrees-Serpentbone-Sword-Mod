3383c533e42149a8063afed58b39bbaaf341ac64
sentree <savageking2006810@gmail.com>
2024-02-18 13:57:42 -0500

    Tweaked Blood Vial

--- src/main/java/net/sentree/sentreesmod/item/ModItems.java
+++ src/main/java/net/sentree/sentreesmod/item/ModItems.java
@@ -13,7 +13,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, SentreesFoodsMod.MOD_ID);
 
     public static final RegistryObject<Item> BLOOD_VIAL = ITEMS.register("blood_vial",
-            () -> new Item(new Item.Properties().food(ModFoods.BLOOD_VIAL)));
+            () -> new Item(new Item.Properties().food(ModFoods.BLOOD_VIAL).stacksTo(20)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/sentree/sentreesmod/item/custom/ModFoods.java
+++ src/main/java/net/sentree/sentreesmod/item/custom/ModFoods.java
@@ -5,7 +5,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public  static final FoodProperties BLOOD_VIAL = new FoodProperties.Builder().alwaysEat().fast().nutrition(0)
-            .saturationMod(0.5f).effect(() -> new MobEffectInstance(MobEffects.HEAL, 20,1),1).build();
+    public  static final FoodProperties BLOOD_VIAL = new FoodProperties.Builder().alwaysEat().meat().nutrition(0)
+            .saturationMod(0).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 10,30),1).build();
 
 }
